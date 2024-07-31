1b6e16444ecfbcae8ade214cc3f59c5163545f56
Joyce-coder-lumia <125268919+Joyce-coder-lumia@users.noreply.github.com>
2024-07-31 15:50:02 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,1 @@
-export const environment = {
-    production: false,
-    firebaseConfig: {
-        apiKey: "AIzaSyA-u9sX02HurPZehtrIFx9ejKdKjltVIno",
-        authDomain: "apicov.firebaseapp.com",
-        projectId: "apicov",
-        storageBucket: "apicov.appspot.com",
-        messagingSenderId: "376398763375",
-        appId: "1:376398763375:web:a3f65f152969213c5ac49a",
-        measurementId: "G-RYGM020DJ5",
-        /*vpaidkey:"BAIyhuUidM2oKdX1w65sUN-FO3_-BAIyhuUidM2oKdX1w65sUN-FO3_-xzgDb6eiR4uN0H8Zot_tUUf8DsNqNkez1h"*/
-      },
-  };
+
